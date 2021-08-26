9cf210ab24b73d00de300e306c2d31ec89f70e34
Oscar Gambini <oscar.gambini@zenmonics.com>
2021-08-25 20:53:00 -0500

    small fixes

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
 output "vpc_id" {
-    value = aws_vpc.vpc.id
+    value       = aws_vpc.vpc.id
     description = "VPC identifier"
 }
 
 output "public_subnets_id" {
     value = aws_subnet.public-subnet.*.id
 }
-
